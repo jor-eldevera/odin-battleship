ed68aeb34b2298e46bd717adca14fbc28f0d7d07
jor-eldevera <jorell.devera@gmail.com>
2023-05-23 19:53:32 -0700

    Write private fields, getters for length and hits, hit, and isSunk

--- Ship.js
+++ Ship.js
@@ -1,6 +1,29 @@
 class Ship {
-    constructor() {
+    #length;
+    #hits;
+    #isSunk;
 
+    constructor(length) {
+        this.#length = length;
+        this.#hits = 0;
+        this.#isSunk = false;
+    }
+
+    getLength() {
+        return this.#length;
+    }
+
+    getHits() {
+        return this.#hits;
+    }
+
+    hit() {
+        if (!this.#isSunk) this.#hits++;
+        if (this.#hits === this.#length) this.#isSunk = true;
+    }
+
+    isSunk() {
+        return this.#isSunk;
     }
 }
 
