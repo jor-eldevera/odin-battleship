d658c4d91c90636db83d04c93d7eca8b7f8a4716
jor-eldevera <jorell.devera@gmail.com>
2023-05-25 17:11:33 -0700

    Test placeShip

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,3 +1,10 @@
+// const { it } = require("jest-circus");
 let { GameBoard } = require("./GameBoard.js");
+// const { default: expect } = require("expect");
 
-let test = new GameBoard();
+let gameboard = new GameBoard();
+
+it("gameboard: place ship", () => {
+    gameboard.placeShip([0, 0], 5);
+    expect(gameboard.getShips()[0].getCoordinates()).toEqual([0, 0]);
+})
