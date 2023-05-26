1a38d97d35574db8ac06d378fbbf5267935a46e7
jor-eldevera <jorell.devera@gmail.com>
2023-05-25 17:11:17 -0700

    Write placeShip

--- GameBoard.js
+++ GameBoard.js
@@ -1,6 +1,20 @@
+const { Ship } = require("./Ship.js");
+
 class GameBoard {
+
+    #ships = [];
+    #misses = [];
+
     constructor() {
 
+    }
+
+    getShips() {
+        return this.#ships;
+    }
+
+    placeShip(coordinates, length) {
+        this.#ships.push(new Ship(coordinates, length));
     }
 }
 
