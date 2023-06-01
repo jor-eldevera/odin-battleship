7d8895f3e969e2b1b4da63922f1c189f32325b9f
jor-eldevera <jorell.devera@gmail.com>
2023-05-31 19:57:35 -0700

    Create ships and switch direction button

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import { Player } from "./Player.js";
 
 const playerOneContainer = document.getElementById("p1-container");
+const shipsContainer = document.getElementById("ships");
+
+const switchDirectionsBtn = document.getElementById("switch-directions-btn");
+let verticalDirection = true; // true if vertical, false if horizontal
 
 let playerOne = new Player();
 let playerTwo = new Player();
@@ -23,7 +27,6 @@ playerTwoBoard.placeShip([1, 10], false, 2);
 
 createGrid();
 function createGrid() {
-    
     for (let i = 0; i <= 10; i++) {
         for (let j = 0; j <= 10; j++) {
             const square = document.createElement("div");
@@ -37,4 +40,74 @@ function createGrid() {
             playerOneContainer.appendChild(square);
         }
     }
+}
+
+switchDirectionsBtn.addEventListener("click", (e) => {
+    if (verticalDirection) {
+        addHorizontalShipsToBottom();
+        verticalDirection = !verticalDirection;
+    } else {
+        addVerticalShipsToBottom();
+        verticalDirection = !verticalDirection;
+    }
+})
+
+addVerticalShipsToBottom();
+function addVerticalShipsToBottom() {
+    removeAllChildNodes(shipsContainer);
+    
+    const patrolVertical = createShip("Ships/patrol_vertical.png", "32px", "62px");
+    shipsContainer.appendChild(patrolVertical);
+
+    const destroyerVertical = createShip("Ships/destroyer_vertical.png", "32px", "93px");
+    shipsContainer.appendChild(destroyerVertical);
+
+    const submarineVertical = createShip("Ships/submarine_vertical.png", "31px", "93px");
+    shipsContainer.appendChild(submarineVertical);
+
+    const battleshipVertical = createShip("Ships/battleship_vertical.png", "31px", "125px");
+    shipsContainer.appendChild(battleshipVertical);
+
+    const carrierVertical = createShip("Ships/carrier_vertical.png", "32px", "155px");
+    shipsContainer.appendChild(carrierVertical);
+}
+
+function addHorizontalShipsToBottom() {
+    removeAllChildNodes(shipsContainer);
+
+    const patrolHorizontal = createShip("Ships/patrol_horizontal.png", "63px", "32px");
+    shipsContainer.appendChild(patrolHorizontal);
+
+    const destroyerHorizontal = createShip("Ships/destroyer_horizontal.png", "93px", "32px");
+    shipsContainer.appendChild(destroyerHorizontal);
+
+    const submarineHorizontal = createShip("Ships/submarine_horizontal.png", "94px", "32px");
+    shipsContainer.appendChild(submarineHorizontal);
+
+    const battleshipHorizontal = createShip("Ships/battleship_horizontal.png", "124px", "32px");
+    shipsContainer.appendChild(battleshipHorizontal);
+
+    const carrierHorizontal = createShip("Ships/carrier_horizontal.png", "155px", "33px");
+    shipsContainer.appendChild(carrierHorizontal);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+/**
+ * Create a ship
+ * @param {String} imageURL is the url of the image
+ * @param {String} width is the width of the image. Must include "px"
+ * @param {String} height is the height of the image. Must include "px"
+ * @returns a div with the background as the imageURL and size width x height
+ */
+function createShip(imageURL, width, height) {
+    const ship = document.createElement("div");
+    ship.style.backgroundImage = `url('${imageURL}')`;
+    ship.style.width = width;
+    ship.style.height = height;
+    return ship;
 }
