3d37f1519596b06bfa8fabc11bcace2de17bda70
jor-eldevera <jorell.devera@gmail.com>
2023-06-02 16:46:38 -0700

    Make ship placement work while removing previously placed ships of either direction

--- script.js
+++ script.js
@@ -36,21 +36,30 @@ function createGrid() {
                 square.classList.add("square");
                 square.addEventListener("click", (e) => {
                     // if it doesn't have children add the overlay
-                    if (!square.firstChild) {
-                        // first detect the ship we're trying to add
-                        // let newShipID = activeShip.id + "-overlay";
+                    if (!square.firstChild && activeShip) {
+                        // first detect the ship we're trying to add (if one is selected)
+                        let shipType = activeShip.id.split("-")[0];
+                        let shipDirection = activeShip.id.split("-")[1];
+                        let newShipID = activeShip.id + "-overlay";
+                        let imageURL = activeShip.style.backgroundImage.slice(5, -2);
+                        let width = activeShip.style.width;
+                        let height = activeShip.style.height;
 
                         // then remove the previous ship if it's already there
-                        if (document.querySelector(".ship-overlay")) {
-                            const parentSquare = document.querySelector(".ship-overlay").parentNode;
-                            parentSquare.removeChild(document.querySelector(".ship-overlay"));
+                        let oldShipHorizontal = document.querySelector("#" + shipType + "-horizontal-overlay");
+                        if (oldShipHorizontal) {
+                            const parentSquare = oldShipHorizontal.parentNode;
+                            parentSquare.removeChild(oldShipHorizontal);
+                        }
+                        let oldShipVertical = document.querySelector("#" + shipType + "-vertical-overlay");
+                        if (oldShipVertical) {
+                            const parentSquare = oldShipVertical.parentNode;
+                            parentSquare.removeChild(oldShipVertical);
                         }
 
                         // finally add the new ship
-                        const shipOverlay = document.createElement('div');
-                        shipOverlay.classList.add("ship-overlay");
-                        square.appendChild(shipOverlay);
-                        console.log("test");
+                        const newShip = createShipForBoardOverlay(newShipID, shipDirection, imageURL, width, height);
+                        square.appendChild(newShip);
                     }
                 })
             }
@@ -83,7 +92,7 @@ shipsContainer.addEventListener("click", (e) => {
             element.style.border = '';
         });
 
-        // Add green border to the clicked element
+        // Add green border to the clicked element7
         selectedElement.style.border = '1px solid green';
     }
 });
@@ -92,55 +101,57 @@ addVerticalShipsToBottom();
 function addVerticalShipsToBottom() {
     removeAllChildNodes(shipsContainer);
     
-    const patrolVertical = createShipForBottom("patrol-vertical", "Ships/patrol_vertical.png", "32px", "62px");
+    const patrolVertical = createShipForBottom("patrol-vertical", "vertical", "Ships/patrol_vertical.png", "32px", "62px");
     shipsContainer.appendChild(patrolVertical);
 
-    const destroyerVertical = createShipForBottom("destroyer-vertical", "Ships/destroyer_vertical.png", "32px", "93px");
+    const destroyerVertical = createShipForBottom("destroyer-vertical", "vertical", "Ships/destroyer_vertical.png", "32px", "93px");
     shipsContainer.appendChild(destroyerVertical);
 
-    const submarineVertical = createShipForBottom("submarine-vertical", "Ships/submarine_vertical.png", "31px", "93px");
+    const submarineVertical = createShipForBottom("submarine-vertical", "vertical", "Ships/submarine_vertical.png", "31px", "93px");
     shipsContainer.appendChild(submarineVertical);
 
-    const battleshipVertical = createShipForBottom("battleship-vertical", "Ships/battleship_vertical.png", "31px", "125px");
+    const battleshipVertical = createShipForBottom("battleship-vertical", "vertical", "Ships/battleship_vertical.png", "31px", "125px");
     shipsContainer.appendChild(battleshipVertical);
 
-    const carrierVertical = createShipForBottom("carrier-vertical", "Ships/carrier_vertical.png", "32px", "155px");
+    const carrierVertical = createShipForBottom("carrier-vertical", "vertical", "Ships/carrier_vertical.png", "32px", "155px");
     shipsContainer.appendChild(carrierVertical);
 }
 
 function addHorizontalShipsToBottom() {
     removeAllChildNodes(shipsContainer);
 
-    const patrolHorizontal = createShipForBottom("patrol-horizontal", "Ships/patrol_horizontal.png", "63px", "32px");
+    const patrolHorizontal = createShipForBottom("patrol-horizontal", "horizontal", "Ships/patrol_horizontal.png", "63px", "32px");
     shipsContainer.appendChild(patrolHorizontal);
 
-    const destroyerHorizontal = createShipForBottom("destroyer-horizontal", "Ships/destroyer_horizontal.png", "93px", "32px");
+    const destroyerHorizontal = createShipForBottom("destroyer-horizontal", "horizontal", "Ships/destroyer_horizontal.png", "93px", "32px");
     shipsContainer.appendChild(destroyerHorizontal);
 
-    const submarineHorizontal = createShipForBottom("submarine-horizontal", "Ships/submarine_horizontal.png", "94px", "32px");
+    const submarineHorizontal = createShipForBottom("submarine-horizontal", "horizontal", "Ships/submarine_horizontal.png", "94px", "32px");
     shipsContainer.appendChild(submarineHorizontal);
 
-    const battleshipHorizontal = createShipForBottom("battleship-horizontal", "Ships/battleship_horizontal.png", "124px", "32px");
+    const battleshipHorizontal = createShipForBottom("battleship-horizontal", "horizontal", "Ships/battleship_horizontal.png", "124px", "32px");
     shipsContainer.appendChild(battleshipHorizontal);
 
-    const carrierHorizontal = createShipForBottom("carrier-horizontal", "Ships/carrier_horizontal.png", "155px", "33px");
+    const carrierHorizontal = createShipForBottom("carrier-horizontal", "horizontal", "Ships/carrier_horizontal.png", "155px", "33px");
     shipsContainer.appendChild(carrierHorizontal);
 }
 
 /**
  * Create a ship
  * @param {String} id is the id of the element
+ * @param {String} direction is the direction of the ship (either "vertical" or "horizontal")
  * @param {String} imageURL is the url of the image
  * @param {String} width is the width of the image. Must include "px"
  * @param {String} height is the height of the image. Must include "px"
  * @returns a div with the background as the imageURL and size width x height
 */
-function createShip(id, imageURL, width, height) {
+function createShip(id, direction, imageURL, width, height) {
     const ship = document.createElement("div");
     ship.id = id;
     ship.style.backgroundImage = `url('${imageURL}')`;
     ship.style.width = width;
     ship.style.height = height;
+    ship.classList.add(direction);
     ship.classList.add("select-ship");
     return ship;
 }
@@ -148,13 +159,14 @@ function createShip(id, imageURL, width, height) {
 /**
  * Create a ship to be used at the bottom of the page when placing ships
  * @param {String} id is the id of the element
+ * @param {String} direction is the direction of the ship (either "vertical" or "horizontal")
  * @param {String} imageURL is the url of the image
  * @param {String} width is the width of the image. Must include "px"
  * @param {String} height is the height of the image. Must include "px"
  * @returns a div with the background as the imageURL, size "width x height", and an event listener which changes the active ship 
 */
-function createShipForBottom(id, imageURL, width, height) {
-    const ship = createShip(id, imageURL, width, height);
+function createShipForBottom(id, direction, imageURL, width, height) {
+    const ship = createShip(id, direction, imageURL, width, height);
 
     ship.addEventListener("click", (e) => {
         activeShip = ship;
@@ -163,9 +175,19 @@ function createShipForBottom(id, imageURL, width, height) {
     return ship;
 }
 
-function createShipForBoardOverlay(id, imageURL, width, height) {
-    const ship = createShip(id, imageURL, width, height);
+/**
+ * Create a ship to be used on the board when placing ships
+ * @param {String} id is the id of the element
+ * @param {String} direction is the direction of the ship (either "vertical" or "horizontal")
+ * @param {String} imageURL is the url of the image
+ * @param {String} width is the width of the image. Must include "px"
+ * @param {String} height is the height of the image. Must include "px"
+ * @returns a div with the background as the imageURL, size "width x height"
+*/
+function createShipForBoardOverlay(id, direction, imageURL, width, height) {
+    const ship = createShip(id, direction, imageURL, width, height);
 
+    return ship;
 }
 
 function removeAllChildNodes(parent) {
