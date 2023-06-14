8f0dbb4bd0f41452db468910be6627fb25a8caa0
jor-eldevera <jorell.devera@gmail.com>
2023-06-13 18:58:59 -0700

    Require ship's image info and ship type when constructing a new Ship. Add getters of shipType and imgInfo

--- Ship.js
+++ Ship.js
@@ -4,19 +4,29 @@ export class Ship {
     #isSunk;
     #coordinates;
     #isVertical;
+    #shipType;
+    #url;
+    #imgWidth;
+    #imgHeight;
 
     /**
      * Construct a new ship
      * @param {Array} coordinates is an array of coordinates in [x, y] form
      * @param {Boolean} isVertical true if ship is vertical, false for horizontal
      * @param {Integer} length is the length of the ship
+     * @param {Array} imgInfo is an array of some info for the img of this ship.
+     * [0] is the url, [1] is the width (px), [2] is the height (px)
      */
-    constructor(coordinates, isVertical, length) {
+    constructor(coordinates, isVertical, length, shipType, imgInfo) {
         this.#length = length;
         this.#hits = 0;
         this.#isSunk = false;
         this.#isVertical = isVertical;
         this.#coordinates = coordinates;
+        this.#shipType = shipType;
+        this.#url = imgInfo[0];
+        this.#imgWidth = imgInfo[1];
+        this.#imgHeight = imgInfo[2]
     }
 
     getLength() {
@@ -49,6 +59,22 @@ export class Ship {
         } else {
             return "horizontal";
         }
+    }
+
+    getURL() {
+        return this.#url;
+    }
+
+    getImgWidth() {
+        return this.#imgWidth;
+    }
+
+    getImgHeight() {
+        return this.#imgHeight;
+    }
+
+    getShipType() {
+        return this.#shipType;
     }
 
     hit() {
